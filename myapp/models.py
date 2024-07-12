59fc7c3f2ce8531115d8dc8fdf7a5dcf1f5801a4
useroqqq <Boris6m@yandex.ru>
2024-07-12 20:23:43 +0300

    12.07.2024 20:23

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     language = models.CharField(max_length=10, choices=[('ru', 'Русский'), ('en', 'English')])
 
