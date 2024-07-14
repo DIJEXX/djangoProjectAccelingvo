010b0ffbf0e7620310c99b777a5dfac44d37c85c
D1JEXX <d1j3xx@gmail.com>
2024-07-14 14:17:13 +0300

    RestoreProject

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,23 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-from .models import User
 
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+from django.forms.widgets import PasswordInput, TextInput
 
-class RegistrationFormEng(forms.ModelForm):
+
+# - Create/Register a user (Model Form)
+
+class CreateUserForm(UserCreationForm):
+
     class Meta:
+
         model = User
-        fields = ['username', 'password']
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+        fields = ['username', 'password1']
+
+
+# - Authenticate a user (Model Form)
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Логин', max_length=100)
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
--- myapp/views.py
+++ myapp/views.py
@@ -1,51 +1,77 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .forms import RegistrationForm, LoginForm
-from .models import User
+
 from django.conf import settings
+from django.shortcuts import render, redirect
+
+from . forms import CreateUserForm, LoginForm
+
+from django.contrib.auth.decorators import login_required
+
+
+# - Authentication models and functions
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
     return render(request, 'index.html')
 
 def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+
+        form = CreateUserForm(request.POST)
+
         if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            return redirect('/')
 
-    else:
-        form = RegistrationForm()
+            form.save()
 
-    return render(request, 'register.html', {'form': form})
+            return redirect("login")
 
-def login(request):
+
+    context = {'registerform':form}
+
+    return render(request, 'register.html', context=context)
+
+
+
+def my_login(request):
+
+    form = LoginForm()
+
     if request.method == 'POST':
+
         form = LoginForm(request, data=request.POST)
-        print(f"Form is valid: {form.is_valid()}")
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            print(f"Authenticated user: {user}")
-            if user is not None:
-                login(request, user)
-                return redirect('main')
-            else:
-                print("Invalid user")
-                print(f"Form errors: {form.errors}")
-    else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form': form})
 
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect("main")
+
+
+    context = {'loginform':form}
+
+    return render(request, 'login.html', context=context)
 def main(request):
     return render(request, 'main.html')
 
+# def login(request):
+#     return render(request, 'login.html')
+
 def difficulty(request):
     return render(request, 'difficulty.html')
-
 def sound(request):
     return render(request, 'sound.html')
 
