bad2b832877134d7eb44e42a45729b015d0684e2
D1JEXX <d1j3xx@gmail.com>
2024-07-14 13:25:34 +0300

    1

--- myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
-    language = models.CharField(max_length=10, choices=[('ru', 'Русский'), ('en', 'English')])
-# com
-    def __str__(self):
-        return self.username
