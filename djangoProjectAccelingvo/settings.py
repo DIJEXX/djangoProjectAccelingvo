84bd774eecbfdf6d6a0137642507bc695da44112
D1JEXX <d1j3xx@gmail.com>
2024-07-17 20:01:12 +0300

    global css style for all pages

--- djangoProjectAccelingvo/settings.py
+++ djangoProjectAccelingvo/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!wvgu!_72a(3xxra$mkwg*aa4)e04244%64ea3n1)1pq$az3kz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
